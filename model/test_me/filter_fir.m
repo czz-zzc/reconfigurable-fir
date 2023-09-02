c0ee06fe15b93e6d38d25653f7cd5b1128eb4e8f
czz-zzc <54883836+czz-zzc@users.noreply.github.com>
2023-09-03 01:09:10 +0800

    Delete model directory

--- model/test_me/filter_fir.m
+++
@@ -1,8 +0,0 @@
-function y = filter_fir(x,coe)
-    l_x=length(x);
-    l_coe=length(coe);
-    y_local = zeros(1,(l_x+l_coe-1));
-    y = zeros(1,l_x);
-    y_local = conv(x,coe);
-    y = y_local(1:l_x);
-end
